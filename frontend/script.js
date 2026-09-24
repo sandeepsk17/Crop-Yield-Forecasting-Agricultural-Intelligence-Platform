2e5bc9f77b51ed88e06dd31249e1a31f2b59fcd4
Sandeep Kumar <sandeepspk1797@gmail.com>
2026-09-24 11:55:59 +0530

    Live api Added

--- frontend/script.js
+++ frontend/script.js
@@ -109,7 +109,7 @@ form.addEventListener("submit", async function (event) {
     try {
 
         const response = await fetch(
-            "http://127.0.0.1:8000/predict",
+            "https://crop-yield-forecasting-agricultural.onrender.com/predict",
             {
                 method: "POST",
 
